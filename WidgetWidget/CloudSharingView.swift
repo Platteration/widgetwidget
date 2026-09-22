ac110781995e48919b3f8488e6fb4f058ba92c75
Platteration <platteration@gmail.com>
2026-09-21 22:05:48 -0400

    Restrict meme share invitations to private access

--- WidgetWidget/CloudSharingView.swift
+++ WidgetWidget/CloudSharingView.swift
@@ -13,7 +13,7 @@ struct CloudSharingView: UIViewControllerRepresentable {
     func makeUIViewController(context: Context) -> UICloudSharingController {
         let controller = UICloudSharingController(share: share, container: container)
         controller.delegate = context.coordinator
-        controller.availablePermissions = [.allowReadWrite]
+        controller.availablePermissions = [.allowPrivate, .allowReadWrite]
         return controller
     }
 
