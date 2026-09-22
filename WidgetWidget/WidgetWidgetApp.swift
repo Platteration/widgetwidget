f62cb5f84ce1412a9e3755d19e7bc80276d3ad6d
Platteration <platteration@gmail.com>
2026-09-21 22:20:41 -0400

    Reload widget from CloudKit push notifications

--- WidgetWidget/WidgetWidgetApp.swift
+++ WidgetWidget/WidgetWidgetApp.swift
@@ -5,9 +5,18 @@ import WidgetKit
 
 extension Notification.Name {
     static let pairDidChange = Notification.Name("pairDidChange")
+    static let cloudKitDidChange = Notification.Name("cloudKitDidChange")
 }
 
 final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        application.registerForRemoteNotifications()
+        return true
+    }
+
     func application(
         _ application: UIApplication,
         userDidAcceptCloudKitShareWith cloudKitShareMetadata: CKShare.Metadata
@@ -19,6 +28,10 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
             guard case .success = result else { return }
 
             SharedStore.savePairRecordID(metadata.rootRecordID)
+            CloudKitPushService.ensureSubscription(
+                for: metadata.rootRecordID,
+                container: container
+            )
             WidgetCenter.shared.reloadAllTimelines()
 
             DispatchQueue.main.async {
@@ -27,6 +40,21 @@ final class AppDelegate: NSObject, UIApplicationDelegate {
         }
 
         container.add(operation)
+    }
+
+    func application(
+        _ application: UIApplication,
+        didReceiveRemoteNotification userInfo: [AnyHashable: Any],
+        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void
+    ) {
+        guard CKNotification(fromRemoteNotificationDictionary: userInfo) != nil else {
+            completionHandler(.noData)
+            return
+        }
+
+        WidgetCenter.shared.reloadTimelines(ofKind: "MemeWidget")
+        NotificationCenter.default.post(name: .cloudKitDidChange, object: nil)
+        completionHandler(.newData)
     }
 }
 
